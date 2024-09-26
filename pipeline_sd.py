e7bdba6951dc9914dc38f97a7ef876e8d8e1f955
Chenyi_Zhuang <74881649+Chenyi-Zhuang@users.noreply.github.com>
2024-09-26 15:12:13 +0800

    Update pipeline_sd.py

--- pipeline_sd.py
+++ pipeline_sd.py
@@ -45,8 +45,8 @@ class MagnetSDPipeline(StableDiffusionPipeline):
         self,
         prompt, 
         pairs=None,
-        alphas=[1, 1],
-        betas=[0.5, 0.5],
+        alphas=None,
+        betas=None,
         K=5,
         alpha_lambda=0.6,
         use_neg = True,
@@ -81,12 +81,18 @@ class MagnetSDPipeline(StableDiffusionPipeline):
             omega = F.cosine_similarity(concept_embeds[:, concept_eid+sd_2].detach().cpu().float(), concept_embeds[:, -1].detach().cpu().float())
 
             if use_pos:
-                alpha = float(torch.exp(alpha_lambda-omega))
+                if alphas is not None:
+                    alpha = alphas[pid]
+                else:
+                    alpha = float(torch.exp(alpha_lambda-omega))
             else:
                 alpha = 0
 
             if use_neg:
-                beta = float(1-omega**2)
+                if betas is not None:
+                    beta = betas[pid]
+                else:
+                    beta = float(1-omega**2)
             else:
                 beta = 0
 
